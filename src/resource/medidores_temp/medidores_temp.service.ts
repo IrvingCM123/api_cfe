87f05d82e4366ae668d5114bd04312e20c6507f4
IrvingCM123 <98989957+IrvingCM123@users.noreply.github.com>
2024-07-18 22:23:25 -0600

    add

--- src/resource/medidores_temp/medidores_temp.service.ts
+++ src/resource/medidores_temp/medidores_temp.service.ts
@@ -32,21 +32,21 @@ export class MedidoresTempService {
     return await this.medidoresRepository.save(createMedidoresTempDto);
   }
 
-  async almacenarfotos( imagen: any,numeroserie: string, user: User_Interface) {
+  async almacenarfotos(imagen: any, numeroserie: string, user: User_Interface) {
 
     validateAll(user);
 
     const correo = user.identificador;
     let medidor = await this.medidoresRepository
-    .createQueryBuilder('medidores_temp')
-    .where('LOWER(medidores_temp.usuario_correo) = LOWER(:correo)', { correo })
-    .andWhere('(medidores_temp.Numero_Serie) = (:numeroserie)', { numeroserie })
-    .getOne();
+      .createQueryBuilder('medidores_temp')
+      .where('LOWER(medidores_temp.usuario_correo) = LOWER(:correo)', { correo })
+      .andWhere('(medidores_temp.Numero_Serie) = (:numeroserie)', { numeroserie })
+      .getOne();
 
     const identificador = (medidor.id).toString();
 
     const resultado: any = await this.transaccionService.transaction(Tipo_Transaccion.Actualizar_Con_Parametros, MedidoresTemp, imagen, 'imagen', identificador);
-   
+
     if (resultado.mensaje === 'Éxito') {
       return {
         status: 201,
@@ -63,39 +63,39 @@ export class MedidoresTempService {
 
   async findAll(user: User_Interface) {
     validateAll(user);
-    
+
     const nDate = new Date().toLocaleString('en-US', {
       timeZone: 'America/Mexico_City'
     });
 
-  // Separa la fecha y la hora
-  const [datePart, timePart] = nDate.split(', ');
+    // Separa la fecha y la hora
+    const [datePart, timePart] = nDate.split(', ');
 
-  // Separa el mes, día y año de la parte de la fecha
-  const [month, day, year] = datePart.split('/');
+    // Separa el mes, día y año de la parte de la fecha
+    const [month, day, year] = datePart.split('/');
 
-  const fecha = `${day}/${month}/${year}`;
+    const fecha = `${day}/${month}/${year}`;
 
     const correo = user.identificador;
     let medidor = await this.medidoresRepository
-    .createQueryBuilder('medidores_temp')
-    .where('LOWER(medidores_temp.usuario_correo) = LOWER(:correo)', { correo })
-    .andWhere('(medidores_temp.status) = (:status)', { status: 'Pendiente' })
-    .andWhere('(medidores_temp.fecha) = (:fecha)', { fecha })
-    .getMany();
+      .createQueryBuilder('medidores_temp')
+      .where('LOWER(medidores_temp.usuario_correo) = LOWER(:correo)', { correo })
+      .andWhere('(medidores_temp.status) = (:status)', { status: 'Pendiente' })
+      .andWhere('(medidores_temp.fecha) = (:fecha)', { fecha })
+      .getMany();
 
     return medidor;
   }
 
   async consultar_Informacion(user: User_Interface) {
     validateAll(user);
-    
+
     const correo = user.identificador;
     let medidor = await this.medidoresRepository
-    .createQueryBuilder('medidores_temp')
-    .where('LOWER(medidores_temp.usuario_correo) = LOWER(:correo)', { correo })
-    .andWhere('(medidores_temp.status) = (:status)', { status: 'Revisado' })
-    .getMany();
+      .createQueryBuilder('medidores_temp')
+      .where('LOWER(medidores_temp.usuario_correo) = LOWER(:correo)', { correo })
+      .andWhere('(medidores_temp.status) = (:status)', { status: 'Revisado' })
+      .getMany();
 
     return medidor;
   }
@@ -104,10 +104,10 @@ export class MedidoresTempService {
     validateAll(user);
     const correo = user.identificador;
     let medidor = await this.medidoresRepository
-    .createQueryBuilder('medidores_temp')
-    .where('LOWER(medidores_temp.usuario_correo) = LOWER(:correo)', { correo })
-    .andWhere('(medidores_temp.Numero_Serie) = (:id)', { id })
-    .getOne();    
+      .createQueryBuilder('medidores_temp')
+      .where('LOWER(medidores_temp.usuario_correo) = LOWER(:correo)', { correo })
+      .andWhere('(medidores_temp.Numero_Serie) = (:id)', { id })
+      .getOne();
     return medidor;
   }
 
@@ -117,16 +117,25 @@ export class MedidoresTempService {
     const correo = user.identificador;
 
     let medidor = await this.medidoresRepository
-    .createQueryBuilder('medidores_temp')
-    .where('(medidores_temp.Numero_Serie) = (:id)', { id })
-    .andWhere('LOWER(medidores_temp.usuario_correo) = LOWER(:correo)', { correo })
-    .getOne();
+      .createQueryBuilder('medidores_temp')
+      .where('(medidores_temp.Numero_Serie) = (:id)', { id })
+      .andWhere('LOWER(medidores_temp.usuario_correo) = LOWER(:correo)', { correo })
+      .getOne();
 
     let identificador = (medidor.id).toString();
 
     const resultado: any = await this.transaccionService.transaction(Tipo_Transaccion.Actualizar_Con_Parametros, MedidoresTemp, estado, 'status', identificador);
-    return {
-      message: 'Se ha actualizado el estado del medidor'
+
+    if (resultado.mensaje === 'Éxito') {
+      return {
+        status: 201,
+        message: 'Se ha actualizado el estado del medidor'
+      }
+    } else {
+      return {
+        status: 400,
+        message: 'Ha ocurrido un error, intentelo de nuevo'
+      }
     }
   }
 
@@ -136,12 +145,12 @@ export class MedidoresTempService {
     const correo = user.identificador;
 
     let medidor = await this.medidoresRepository
-    .createQueryBuilder('medidores_temp')
-    .where('LOWER(medidores_temp.Numero_Serie) = LOWER(:Numero_Serie)', { Numero_Serie })
-    .andWhere('LOWER(medidores_temp.usuario_correo) = LOWER(:correo)', { correo })
-    .getOne();
-  
-    
+      .createQueryBuilder('medidores_temp')
+      .where('LOWER(medidores_temp.Numero_Serie) = LOWER(:Numero_Serie)', { Numero_Serie })
+      .andWhere('LOWER(medidores_temp.usuario_correo) = LOWER(:correo)', { correo })
+      .getOne();
+
+
     return this.medidoresRepository.delete(medidor.id);
   }
 }
